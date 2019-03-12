3d93d353b4ff1af036b4cc7d3ebcdfc642f451ff
Yingqi Chen <chanwk18@hotmail.com>
2019-03-12 05:57:01 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,13 +5,13 @@
 def position_taken? (board, index)
   if (board[index]=="X")||(board[index]=="O")
     true
-  else false
+#  the "else false" doesn't necessarily have to be put here because without it, the method will return "nil", which is considered to be falsey too
   end
 end
 
 def valid_move?(board, index)
   if (index.between?(0,8))
-    if !(position_taken?(board,index))
+    if (!position_taken?(board,index))
       return true
     end
 
