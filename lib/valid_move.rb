d298e916802c2a2a95561a9eb8733a5e78f875ab
Connor Cowan <ccowan003@comcast.net>
2019-05-15 21:41:41 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 if(index.to_i.between?(1,9)) #if index is within available positions on board
-  if !(position_taken?(board,index)) #if our position is not taken
+  if !(position_taken?(board,index)) #if our position is not taken (call to our helper method position_taken?)
     return true                       # return true for valid move
   end
 end
