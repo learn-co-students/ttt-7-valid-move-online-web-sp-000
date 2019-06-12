9f8ca870ec989cf9afdc0311eee7ae42327cb231
Peter Grant <jgrantps@hotmail.com>
2019-06-12 16:50:27 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,21 @@
 # code your #valid_move? method here
 
+puts "choose a number in between 1 and 9:"
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+#input = gets.strip
+#number = input.to_i - 1
+number = 3
+
+def position_taken?(board, index)
+  !(board[index] == "" || board[index] == " " || board[index] == nil)
+end
+
+def valid_move?(board, number)
+  number.between?(0,8) && !(position_taken?(board, number))
+end
+
+puts valid_move?(board, number)
+
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
