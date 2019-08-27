29367203f1f2aee12e2e7e6cccb7bd4129b3c79f
Blythe Pollard <blytheepollard@gmail.com>
2019-08-27 01:06:42 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,20 @@
 # code your #valid_move? method here
-def valid_move?
-  if position_taken?(board, user_input)
+def valid_move?(board, user_input)
+  if user_input != user_input.clamp(0,8)
+    false
+  else position_taken?(board, user_input) 
+end 
+end
 
 
 def position_taken?(board, user_input)
   if board[user_input] == " " || board[user_input] == "" || board[user_input] == nil
-    false
-  else 
     true
+  else false
 end
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+#user_input != 
+#else user_input != user_input.clamp(0,8)
+    #false
