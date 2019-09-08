514858adba2613c5d2580fa6003580a16b7fbd4b
Kathia Mgwaba <mgwaba68@gmail.com>
2019-09-08 14:31:21 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,11 @@
-# code your #valid_move? method h
+# code your #valid_move? method Here
 def valid_move?(board, index)
-  if position_taken?(board[index])
+  if position_taken?(board, index)
     return false
   elsif index.between?(0, 8)
     return true
   end
+end
 
 
 
@@ -16,7 +17,5 @@ def position_taken?(board, index)
   elsif (board[index] == "X" || board[index] == "O")
 
     return true
-
   end
-end
 end
