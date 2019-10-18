f9cd29b93ccd002f357d65004434a26539488981
Sweet Van Loan <sweet@sweetvanloan.com>
2019-10-18 20:04:41 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,14 @@
 # code your #valid_move? method here
 
+def valid_move? (board, index)
+  if between (0, 8) = FALSE
+    put "Not a Valid Move!"
+    
+    
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?
+  if index != nil
+    index = FALSE
+  
