3e4240321119a30cc2c2881dd0ddeb9c593ee81d
Josiah  Partridge <jpartridj94@gmail.com>
2020-01-28 03:47:39 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  if position_taken?(board, index) == true
+    puts "That position is already populated with a value. Please try again"
+  elsif index.between?(0, 8) == false
+    puts "That is an invalid input. Please enter a value between 1 and 9"
+  else
+    true
+  end
+end
 
+def position_taken?(board, index)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
