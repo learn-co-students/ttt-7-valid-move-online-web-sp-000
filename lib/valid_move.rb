1f8823e45bce42785710c450aeb42ee69c9299dc
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-14 19:34:56 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,15 @@
+require_relative '../spec/valid_move_spec.rb'
+#require_relative '../spec/spec_helper.rb'
+
 # code your #valid_move? method here
+def valid_move?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    false 
+    elsif board[index] == " " || board[index] == ""
+    true 
+  end
+end  
+
+# re-define your #position_taken? method here
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
