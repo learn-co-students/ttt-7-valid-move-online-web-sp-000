51fa515971a88d475cc990d07fc22ee0ead52d82
Duong Dang <dddangmedia@gmail.com>
2020-02-23 06:20:04 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,18 @@
 
-def valid_move?(board, number_entered)
-  number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
+=begin
+  def valid_move?(board, number_entered)
+    number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
+  end
+  
+  def position_taken?(board, index)
+   !(board[index] == " " || board[index] == "" || board[index] == nil)
+  end
+=end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && (position_taken?(board, index))
 end
 
 def position_taken?(board, index)
- !(board[index] == " " || board[index] == "" || board[index] == nil)
+  (board[index] == " " || board[index] == "" || board[index] == nil)
 end
