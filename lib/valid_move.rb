24ce438a77d711987e9baadc2a338de676efbdc3
immutable-integer-8885 <gidelbusto@gmail.com>
2020-04-11 23:15:50 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,32 @@
 # code your #valid_move? method here
 
+def valid_move?(board,index)
+  if index.between?(0,8)
+    if  position_taken?(board,index)
+      false
+    else
+      true
+    end
+  else
+    false
+  end
+end
+
+#returns nil or false for a position that is not on the board
+#returns true for a valid position on a non-empty board
+#returns nil or false for an occupied position
+#returns true for a valid position on an empty board 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken?(board,index)
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  else board[index] == "X" or board[index] == "O" 
+    true
+  end 
+end
