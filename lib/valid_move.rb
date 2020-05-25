3abe0e2bf0775861269f4c7991c8e1e39da77fdc
strinie <hello@katherinesalamat.com>
2020-05-25 18:37:18 -0400

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-    def position_taken?(array, ind)
-      if array[ind] == " " || array[ind] == "" || array[ind] == nil
+    def position_taken?(array, index)
+      if array[index] == " " || array[index] == "" || array[index] == nil
         return false
       else
         return true
