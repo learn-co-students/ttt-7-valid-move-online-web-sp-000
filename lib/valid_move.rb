9a4ddc2d447fa428b1a170ae53cee0491532c330
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-08-11 21:23:48 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
-# code your #valid_move? method here
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+# Decides whether a user's move is valid
+def valid_move?(array, index)
+  if index > 8
+    return false
+  else if position_taken?(array, index) == false
+    return true
+  else
+    return false
+  end
+end
+end
+# Decides whether a position on the board is already occupided
+def position_taken?(array, index)
+  if array[index] == " " || array[index] == "" || array[index] == nil
+    return false
+  else if array[index] == "X" || array[index] == "O"
+    return true
+  end
+  end
+end
