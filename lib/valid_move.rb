29104ee04bc2af68b12f9b1460e4653682a378a5
Chris Benedict <cabx88@gmail.com>
2020-10-02 10:01:29 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,17 @@
-# code your #valid_move? method here
+def position_taken?(board, input)
+  if board[input] == "X" || board[input] == "O"
+    return true
+  else
+    return false
+  end
+end
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, input)
+  if input < 0 || input > 9 
+    return false
+  elsif position_taken?(board, input) == true
+    return false
+  else
+    return true
+  end
+end
