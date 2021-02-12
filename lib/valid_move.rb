887c3f3f9c873506e225fada5e1f2fffd8290c69
Kassandra Guerrero <kassandra-g@hotmail.com>
2021-02-12 19:25:32 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,10 @@
-def valid_move?(board, index)
-  if index.between?(0, 8) && !(position_taken?(board, index))
+def valid_move?(board,index)
+  if index.between?(0,8) && !(position_taken?(board,index))
     return true
   else
     false
   end
 end
-
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == nil
