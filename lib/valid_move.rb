b21f611bfcbde8bcb072fefed0ab8292167e0933
Ian King <kickthechair@gmail.com>
2021-05-11 17:22:02 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,5 @@
 # code your #valid_move? method here
+#valid_move? - uses the helper method #position_taken to ensure the position on the board hasn't been played yet and also ensures a valid space on the board is actually played between spaces 1 - 9 (this is actually 0 - 8 because the array index is 1 less as it starts at 0)
 def valid_move?(board, index)
   if position_taken?(board, index) == false && index.between?(0, 8)
     true
@@ -19,7 +20,7 @@ end
 
 
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method.
 
 def position_taken?(board, index) 
   board[index] == " " || board[index] == "" || board[index] == nil ? false : true 
